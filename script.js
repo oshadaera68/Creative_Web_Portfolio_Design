eef5cb75f18a400f47b66b38b7f0d7fca128877f
oshadaera68 <mindartlk010@gmail.com>
2023-02-13 10:33:39 +0530

    review section completed

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let menu = document.querySelector('.menu-icon')
 menu.onClick = () => {
     menu.classList.toggle("move")
 };
+
+// reviews swiper
+var swiper = new Swiper(".reviews-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
